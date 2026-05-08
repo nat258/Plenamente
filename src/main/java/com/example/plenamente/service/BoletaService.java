59e5ea674becbeadee5efb8af769bf77fa5bb997
Nataly Arriaza <nat.arriaza@duocuc.cl>
2026-05-07 23:42:06 -0400

    Agrega Service recuperado y actualiza estructura de DTOs

--- src/main/java/com/example/plenamente/service/BoletaService.java
+++ src/main/java/com/example/plenamente/service/BoletaService.java
@@ -1,20 +1,81 @@
 package com.example.plenamente.service;
-
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.example.plenamente.DTO.BoletaDTO;
 import com.example.plenamente.model.Boleta;
-
+import com.example.plenamente.repository.BoletaRepository;
 import jakarta.transaction.Transactional;
 
 @Service
 @Transactional
 public class BoletaService {
+    @Autowired
+    private BoletaRepository boletaRepository;
 
+    public List<BoletaDTO>obtenerBoletas(){
+        return boletaRepository.findAll().stream()
+            .map(this::convertirADTO)
+            .toList();
+    }
 
+    public BoletaDTO buscarPorId(Integer id){
+        Boleta boleta = boletaRepository.findById(id)
+            .orElseThrow(()-> new RuntimeException("¡Boleta no encontrada!"));
+        return convertirADTO(boleta);
+    }
 
+    public String eliminar(Integer id){
+        try{
+            Boleta boleta = boletaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("No es posible eliminar boleta con ID" + id +"No existe"));
+            boletaRepository.delete(boleta);
+            return"La boleta'" + boleta.getId()+"'ha sido eliminada exitosamente";
+        }catch(RuntimeException b){
+            return b.getMessage();
+        }
+    }
 
-    //convertir a dto
+    public Boleta guardarBoleta(Boleta boleta){
+        return boletaRepository.save(boleta);
+    }
+
+    public Boleta  actualizarBoleta(Integer id, Boleta boleta){
+        Boleta bole = boletaRepository.findById(id).orElseThrow(() -> new RuntimeException("La boleta no existe en los registros"));
+        if(boleta.getTipoPago() != null){
+            bole.setTipoPago(boleta.getTipoPago());
+        }
+        if(boleta.getMonto() != null){
+            bole.setMonto(boleta.getMonto());
+        }
+        if(boleta.getReservaHora()!= null){
+            bole.setReservaHora(boleta.getReservaHora());
+        }
+        return boletaRepository.save(bole);
+    }
+
+    //Busqueda por tipo de pago  
+    public List<BoletaDTO>BuscarPorTipoPago(String pago){
+        return boletaRepository.findByTipoPago(pago).stream()
+                .map(this::convertirADTO)
+                .toList();
+    }
+
+    //Busqueda por monto exacto . 
+    public List<BoletaDTO>BuscarMontoExacto(Integer monto){
+        return boletaRepository.findByMonto(monto).stream()
+                .map(this::convertirADTO)
+                .toList();        
+    }
+    
+    //Busqueda por monto superior 
+    public List<BoletaDTO>porMontoSuperior(Integer nivelMinimo){
+        return boletaRepository.PorMontoSuperior(nivelMinimo).stream()
+                 .map(this::convertirADTO)
+                 .toList();
+    }
+
+    //Convertir a DTO
     private BoletaDTO convertirADTO(Boleta boleta) {
     BoletaDTO dto = new BoletaDTO();
     dto.setId(boleta.getId());
@@ -25,6 +86,8 @@ public class BoletaService {
         dto.setReservaHoraId(boleta.getReservaHora().getId());
     }
     return dto;
-}
+    }
+
+
 
 }
--- src/main/java/com/example/plenamente/service/ComunaService.java
+++ src/main/java/com/example/plenamente/service/ComunaService.java
@@ -2,7 +2,9 @@ package com.example.plenamente.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.plenamente.DTO.BoletaDTO;
 import com.example.plenamente.DTO.ComunaDTO;
+import com.example.plenamente.model.Boleta;
 import com.example.plenamente.model.Comuna;
 
 import jakarta.transaction.Transactional;
@@ -10,6 +12,17 @@ import jakarta.transaction.Transactional;
 @Service
 @Transactional
 public class ComunaService {
+
+
+
+    public ComunaDTO buscarPorId(Integer id){
+        Comuna comuna = comunaRepository.findById(id)
+            .orElseThrow(()-> new RuntimeException("¡Comuna no encontrada!"));
+        return convertirADTO(comuna);
+    }
+
+
+
 
 
 
@@ -23,6 +36,5 @@ public class ComunaService {
         dto.setNombreRegion(comuna.getRegion().getNombre());
     }
     return dto;
-}
-
+    }
 }
